1aed8a125b3341798791ab19f2f7dc5ef440465c
cavaliere <davincicavaliere@gmail.com>
2025-04-02 08:43:37 -0500

    lista de tareas con angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { TareasService } from './services/tareas.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'todo-app-angular';
+export class AppComponent implements OnInit {
+
+  listaTareas: string[] = [];
+  nuevaTarea: string = '';
+
+  private _tareasService = inject(TareasService);
+
+  ngOnInit(): void {
+    this.listaTareas = this._tareasService.getTareas();
+  }
+
+  agregarTarea() {
+    this._tareasService.agregarTarea(this.nuevaTarea);
+    this.nuevaTarea = '';
+    this.listaTareas = this._tareasService.getTareas();
+  }
+
+  eliminarTarea(index: number) {
+    this._tareasService.eliminarTarea(index);
+    this.listaTareas = this._tareasService.getTareas();
+  }
+
+
 }
